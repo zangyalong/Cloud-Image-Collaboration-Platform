51d615e45d64e0a61ecc6a4390a9ead0deb6f974
李先生 <14866162+zang3842@user.noreply.gitee.com>
2025-07-30 09:52:41 +0800

    补充第一次提交记录

--- src/main/java/com/zangyalong/mingzangpicturebackend/config/CorsConfig.java
+++ src/main/java/com/zangyalong/mingzangpicturebackend/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.zangyalong.mingzangpicturebackend.mapper;
+package com.zangyalong.mingzangpicturebackend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
